1d66c101c60192672bc2dc4ca9ead23d736a19fe
Fatih Güleşen <fatihgulesen96@gmail.com>
2026-04-23 16:29:24 +0000

    feat: clean and cast columns in staging models

--- models/staging/stg_product.sql
+++ models/staging/stg_product.sql
@@ -1,1 +1,12 @@
-select * from {{ source('raw', 'product') }}
+WITH product AS (
+    SELECT * FROM {{ source('raw', 'product') }}
+),
+
+renamed AS (
+    SELECT
+        products_id,
+        CAST(purchse_price AS FLOAT64) AS purchase_price -- Hem tipini düzelttik hem adını
+    FROM product
+)
+
+SELECT * FROM renamed
